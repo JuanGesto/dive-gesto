1827e74c8db3206ff37f5322134eb782053b18ee
Juan Gesto <juangesto2811@gmail.com>
2022-10-10 15:06:01 -0300

    empty item list to home page button

--- src/components/Main/ItemListContainer/ItemList.js
+++ src/components/Main/ItemListContainer/ItemList.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Item from "./Item";
+import { Link } from "react-router-dom";
 
 const ItemList = ({ items }) => {
     return (
         <>
-            {items.map((item) => {
-                return (
-                    <Item item={item} key={item.id}/>
-                );
-            })}
+            {items.length === 0 ? (
+                <div id="noProducts">
+                    <h3>There's nothing here</h3>
+                    <Link to={"/"}>
+                        <button className="btn btn-light">
+                            Go back to home page
+                        </button>
+                    </Link>
+                </div>
+            ) : (
+                items.map((item) => {
+                    return <Item item={item} key={item.id} />;
+                })
+            )}
         </>
     );
 };
